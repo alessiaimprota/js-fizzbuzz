46a23c352e594c1828b5bdc8736174a135c88d82
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-02 17:49:55 +0100

    add css

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-//Scrivi un programma che stampi i numeri da 1 a 100,
+
+let result="";
+
 for (i = 1; i <= 100; i++) {
   //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
   if (i % 5 === 0 && i % 3 === 0) {
     console.log("fizzbuzz");
+    result+=`<div class="btn-fizzbuzz btn"> &#9734; &#9733;</div>`
     //ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
   } else if (i % 3 === 0) {
     console.log("fizz");
+    result+=`<div class="btn-fizz btn">&#9734;</div>`
   } else if (i % 5 === 0) {
     console.log("buzz");
+    result+=`<div class="btn-buzz btn">&#9733;</div>`
     //altrimenti stampo il i normale
   } else {
     console.log(i);
+    result+=`<div class="btn"> ${i}</div>`
   }
 }
+
+
+
+document.getElementById("game-result").innerHTML = result;
